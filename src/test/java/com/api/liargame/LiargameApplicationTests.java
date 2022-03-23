0c17ce9bd09e2f94fd6a9c09c5a54135477b7779
jjonyo <jonghyuk96@cau.ac.kr>
2022-03-23 14:26:12 +0900

    Initial Commit

---
+++ src/test/java/com/api/liargame/LiargameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.liargame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiargameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
