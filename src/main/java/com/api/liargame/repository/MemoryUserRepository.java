20db8c5e96d25fbdda9ba92e001544a7507bdea5
jjonyo <jonghyuk96@cau.ac.kr>
2022-03-31 14:42:26 +0900

    FEAT : (#5) User, GameRoom Repository & test

---
+++ src/main/java/com/api/liargame/repository/MemoryUserRepository.java
@@ -0,0 +1,38 @@
+package com.api.liargame.repository;
+
+import com.api.liargame.domain.User;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MemoryUserRepository implements UserRepository {
+
+  List<User> users = new LinkedList<>();
+
+  @Override
+  public String save(User user) {
+    users.add(user);
+    return user.getId();
+  }
+
+  @Override
+  public void delete(String id) {
+    User user = findById(id);
+
+    if (user == null) return;
+
+    users.remove(user);
+  }
+
+  @Override
+  public User findById(String id) {
+    return users.stream()
+        .filter(u -> u.getId().equals(id))
+        .findAny()
+        .orElse(null);
+  }
+
+  @Override
+  public List<User> findAll() {
+    return users;
+  }
+}
---
+++ src/test/java/com/api/liargame/repository/MemoryUserRepositoryTest.java
@@ -0,0 +1,54 @@
+package com.api.liargame.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.api.liargame.domain.User;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MemoryUserRepositoryTest {
+
+  UserRepository userRepository = new MemoryUserRepository();
+
+  @Test
+  @DisplayName("유저를 저장할 수 있어야 한다.")
+  void save() {
+    User user1 = User.builder()
+        .nickname("user1")
+        .character("c1")
+        .build();
+
+    User user2 = User.builder()
+        .nickname("user2")
+        .character("c2")
+        .build();
+
+    String savedId = userRepository.save(user1);
+    userRepository.save(user2);
+    User savedUser = userRepository.findById(savedId);
+    List<User> users = userRepository.findAll();
+
+    assertThat(savedUser.getNickname()).isEqualTo(user1.getNickname());
+    assertThat(savedId).isEqualTo(user1.getId());
+    assertThat(users.size()).isEqualTo(2);
+  }
+
+  @Test
+  @DisplayName("유저를 삭제할 수 있어야 한다.")
+  public void delete() {
+    User user1 = User.builder()
+        .nickname("user1")
+        .character("c1")
+        .build();
+
+    userRepository.save(user1);
+    userRepository.delete(user1.getId());
+    User foundUser = userRepository.findById(user1.getId());
+    List<User> users = userRepository.findAll();
+
+    assertThat(foundUser).isNull();
+    assertThat(users.size()).isEqualTo(0);
+  }
+
+}
