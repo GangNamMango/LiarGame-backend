d93e8b94cd20f94af2ddb34a70837bcbc6289ff4
jjonyo <jonghyuk96@cau.ac.kr>
2022-03-31 14:42:26 +0900

    FEAT : (#5) User, GameRoom Repository & test

---
+++ src/main/java/com/api/liargame/repository/MemoryGameRoomRepository.java
@@ -0,0 +1,40 @@
+package com.api.liargame.repository;
+
+import com.api.liargame.domain.GameRoom;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemoryGameRoomRepository implements GameRoomRepository{
+
+  private final Map<String, GameRoom> gameRoomMemory = new HashMap<>();
+
+  @Override
+  public String save(GameRoom gameRoom) {
+    gameRoomMemory.put(gameRoom.getRoomId(), gameRoom);
+
+    return gameRoom.getRoomId();
+  }
+
+  @Override
+  public void delete(String roomId) {
+    GameRoom gameRoom = findById(roomId);
+
+    if (gameRoom == null) return;
+
+    gameRoomMemory.remove(roomId);
+  }
+
+  @Override
+  public GameRoom findById(String roomId) {
+    return gameRoomMemory.get(roomId);
+  }
+
+  @Override
+  public List<GameRoom> findAll() {
+    return new ArrayList<>(gameRoomMemory.values());
+  }
+}
---
+++ src/test/java/com/api/liargame/repository/MemoryGameRoomRepositoryTest.java
@@ -0,0 +1,81 @@
+package com.api.liargame.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.api.liargame.domain.GameRoom;
+import com.api.liargame.domain.Setting;
+import com.api.liargame.domain.User;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MemoryGameRoomRepositoryTest {
+
+  GameRoomRepository gameRoomRepository = new MemoryGameRoomRepository();
+
+  @Test
+  @DisplayName("게임 방을 저장할 수 있어야 한다.")
+  void save() {
+    User user1 = createUser("user1", "ch1");
+    User user2 = createUser("user2", "ch2");
+    Setting setting = new Setting();
+
+    GameRoom gameRoom = GameRoom.builder()
+        .host(user1)
+        .settings(setting)
+        .build();
+
+    gameRoom.addUser(user2);
+
+    String roomId = gameRoomRepository.save(gameRoom);
+    GameRoom savedGameRoom = gameRoomRepository.findById(roomId);
+
+    assertThat(roomId).isEqualTo(gameRoom.getRoomId());
+    assertThat(savedGameRoom.getUsers().size()).isEqualTo(2);
+    assertThat(savedGameRoom.getHost()).isSameAs(user1);
+  }
+
+  @Test
+  @DisplayName("게임 방을 찾을 수 있어야 한다.")
+  void findById() {
+    User user1 = createUser("user1", "ch1");
+    Setting setting = new Setting();
+
+    GameRoom gameRoom = GameRoom.builder()
+        .host(user1)
+        .settings(setting)
+        .build();
+
+    gameRoomRepository.save(gameRoom);
+
+    GameRoom foundGameRoom = gameRoomRepository.findById(gameRoom.getRoomId());
+
+    assertThat(foundGameRoom).isSameAs(gameRoom);
+    assertThat(gameRoomRepository.findById("xxx")).isNull();
+  }
+
+  @Test
+  @DisplayName("게임 방을 삭제할 수 있어야 한다.")
+  public void delete() {
+    User user1 = createUser("user1", "ch1");
+    Setting setting = new Setting();
+
+    GameRoom gameRoom = GameRoom.builder()
+        .host(user1)
+        .settings(setting)
+        .build();
+
+    String roomId = gameRoomRepository.save(gameRoom);
+    gameRoomRepository.delete(roomId);
+
+    assertThat(gameRoomRepository.findById(roomId)).isNull();
+    assertThat(gameRoomRepository.findAll().size()).isEqualTo(0);
+  }
+
+  public User createUser(String nickname, String character) {
+    return User.builder()
+        .nickname(nickname)
+        .character(character)
+        .build();
+  }
+}
