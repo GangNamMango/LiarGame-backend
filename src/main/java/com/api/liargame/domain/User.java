3138ed0fb5aa015b744cf664a820709371fccd0c
jjonyo <jonghyuk96@cau.ac.kr>
2022-03-31 11:06:00 +0900

    FEAT : (#5) Add Role, GameRole enum in User domain

--- src/main/java/com/api/liargame/domain/User.java
+++ src/main/java/com/api/liargame/domain/User.java
@@ -7,16 +7,25 @@ import lombok.Getter;
 
 @Getter
 public class User {
+  public enum Role {
+    HOST,
+    GUEST
+  }
+  public enum GameRole {
+    LIAR,
+    MEMBER
+  }
+
   private final String id;
   private String nickname;
-  private String role;
-  private String gameRole;
+  private Role role;
+  private GameRole gameRole;
   private String character;
   private final LocalDateTime createdAt;
   private LocalDateTime updatedAt;
 
   @Builder
-  public User(String nickname, String role, String gameRole, String character) {
+  public User(String nickname, Role role, GameRole gameRole, String character) {
     this.id = UUID.randomUUID().toString();
     this.nickname = nickname;
     this.role = role;
@@ -30,11 +39,11 @@ public class User {
     this.nickname = nickname;
   }
 
-  public void setRole(String role) {
+  public void setRole(Role role) {
     this.role = role;
   }
 
-  public void setGameRole(String gameRole) {
+  public void setGameRole(GameRole gameRole) {
     this.gameRole = gameRole;
   }
 
